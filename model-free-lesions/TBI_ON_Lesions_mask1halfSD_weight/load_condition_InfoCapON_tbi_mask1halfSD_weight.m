f571f320bdadd8ce918edff1e4d42425f2e6ab4e
Noelia Martinez Molina <38212657+noechan@users.noreply.github.com>
2023-07-11 13:02:29 +0200

    Delete model-free-lesions directory

--- model-free-lesions/TBI_ON_Lesions_mask1halfSD_weight/load_condition_InfoCapON_tbi_mask1halfSD_weight.m
+++
@@ -1,8 +0,0 @@
-function [vcondition]=load_condition_InfoCapON_tbi_mask1halfSD_weight(trial,cond)
-
-for i=1:trial
-
-    load(sprintf(['Wtrials_%03d_%d_err_hete_ON_tbi_mask1halfSD_weight.mat'],i,cond));
-    vcondition.infocap_all(i,:)=infocapacity;
-    vcondition.suscep_all(i,:)=susceptibility;
-end
