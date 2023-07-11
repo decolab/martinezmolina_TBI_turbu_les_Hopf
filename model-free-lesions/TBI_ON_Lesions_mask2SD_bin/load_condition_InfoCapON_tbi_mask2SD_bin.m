1b1b279accd9cff03d8ad91aa87c04221a16d96e
Noelia Martinez Molina <38212657+noechan@users.noreply.github.com>
2023-07-11 12:28:57 +0200

    Add files via upload

---
+++ model-free-lesions/TBI_ON_Lesions_mask2SD_bin/load_condition_InfoCapON_tbi_mask2SD_bin.m
@@ -0,0 +1,8 @@
+function [vcondition]=load_condition_InfoCapON_tbi_mask2SD_bin(trial,cond)
+
+for i=1:trial
+
+    load(sprintf('Wtrials_%03d_%d_err_hete_ON_tbi_mask2SD_bin.mat',i,cond));
+    vcondition.infocap_all(i,:)=infocapacity;
+    vcondition.suscep_all(i,:)=susceptibility;
+end
