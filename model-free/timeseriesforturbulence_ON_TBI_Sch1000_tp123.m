7f57170844f3d046259e4f4628a7f2be908a90a3
Noelia Martinez Molina <38212657+noechan@users.noreply.github.com>
2023-09-18 10:57:51 +0200

    Add files via upload

---
+++ model-free/timeseriesforturbulence_ON_TBI_Sch1000_tp123.m
@@ -0,0 +1,28 @@
+%% Create timeseries for turbulence
+
+clear all
+addpath('/Volumes/LASA/TBI_OpenNeuro/Shared_code/Turbulence/TBI_turbulence_current_code') %code path
+data_path='/Volumes/LASA/TBI_OpenNeuro/TBI_openneuro/timeseries/preprocessing/tbi_sch1000/';
+input_path='/Volumes/LASA/TBI_OpenNeuro/TBI_openneuro/timeseries/inputs_sch1000/';
+fnames=dir(data_path);
+fnames=fnames(~ismember({fnames.name},{'.','..'}));
+
+for s=1:12
+    load(fullfile(data_path,fnames(s).name))
+    for i=1:1000 %1000 Schaefer nodes
+        data_sc1000_openneuro_tbi_tp1(i, 1:144)=data{1,167+i}(1:144)';
+        data_sc1000_openneuro_tb_tp2(i, 1:144)=data{1,167+i}(145:288)';
+        data_sc1000_openneuro_tb_tp3(i, 1:144)=data{1,167+i}(289:end)';
+    end
+
+tseries_tp1(s, 1)={data_sc1000_openneuro_tbi_tp1};
+tseries_tp2(s, 2)={data_sc1000_openneuro_tb_tp2};
+tseries_tp3(s, 3)={data_sc1000_openneuro_tb_tp3};
+    
+    clearvars -except data_path input_path fnames tseries_tp1 tseries_tp2 tseries_tp3
+end
+
+cd(input_path)
+save('tseries_openneuro_tbi_tp1_sch1000_test.mat', 'tseries_tp1')
+save('tseries_openneuro_tbi_tp2_sch1000_test.mat', 'tseries_tp2')
+save('tseries_openneuro_tbi_tp3_sch1000_test.mat', 'tseries_tp3')
