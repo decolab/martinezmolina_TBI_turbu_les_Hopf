1193c90bb7e73ed6ae4ff82c1588ad64d5e57032
Noelia Martinez Molina <38212657+noechan@users.noreply.github.com>
2023-09-22 11:22:07 +0200

    Add files via upload

---
+++ model-free/Schaefer400/timeseriesforturbulence_openneuro_sch400_hc_tp2.m
@@ -0,0 +1,21 @@
+%% Create timeseries for turbulence
+
+clear all
+data_path='/Volumes/LASA/TBI_OpenNeuro/TBI_openneuro/timeseries/preprocessing/hc_tp2_sch400/';
+input_path='/Volumes/LASA/TBI_OpenNeuro/TBI_openneuro/timeseries/inputs_sch400/';
+fnames=dir(data_path);
+fnames=fnames(~ismember({fnames.name},{'.','..'}));
+
+for s=1:12
+    load(fullfile(data_path,fnames(s).name))
+    for i=1:400 %100 Schaefer nodes
+        data_sc400_openneuro_hc_tp2(i, 1:144)=data{1,1267+i}(1:144)';
+ 
+    end
+
+tseries(s, 1)={data_sc400_openneuro_hc_tp2};
+    clearvars -except data_path input_path fnames tseries
+end
+
+cd(input_path)
+save('tseries_openneuro_hc_tp2_sch400.mat', 'tseries')
